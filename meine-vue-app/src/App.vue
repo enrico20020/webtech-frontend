af1cf5d0cf39f0608f8398ca6cda634b16eaba07
macbook <enricoschweigert@gmail.com>
2025-11-07 21:09:37 +0100

    Notizenliste mit v-for gerendert (Auf. 2)

---
+++ meine-vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import NoteItem from './components/Noteitem.vue';
+import {ref} from 'vue';
+
+const notes = ref([
+  { id: 1, text: 'Tomaten' },
+  { id: 2, text: 'Gurken' },
+  { id: 3, text: 'Paprika' },
+]);
+</script>
+
+<template>
+  <header>
+    <h1>Meine Vue Notizenliste</h1>
+  </header>
+
+  <main class="notes-content">
+    <div
+      v-for="note in notes":key="note.id" >
+      <NoteItem :note-data="note" />
+    </div>
+  </main>
+</template>
+
---
+++ meine-vue-app/src/components/Noteitem.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {defineProps} from 'vue';
+const props = defineProps({noteData: {type: Object, required: true}});
+</script>
+
+<template>
+ <div class="note-item" >
+   <p>{{ noteData.text }}</p>
+ </div>
+</template>
+
