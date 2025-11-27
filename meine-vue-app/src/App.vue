5f6ec7b418ec2700e54f84c43db7a9f000308098
macbook <enricoschweigert@gmail.com>
2025-11-27 22:20:57 +0100

    Aufgabe 3 Frontend

--- meine-vue-app/src/App.vue
+++ meine-vue-app/src/App.vue
@@ -2,21 +2,26 @@
 import NoteItem from './components/Noteitem.vue';
 import {ref} from 'vue';
 
+const Backend_URL = 'https://webtech-backend-6ot9.onrender.com'
+
 const notes = ref([
-  { id: 1, text: 'Tomaten' },
-  { id: 2, text: 'Gurken' },
-  { id: 3, text: 'Paprika' },
-]);
+  { id: 1, text: 'Laptop' },
+  { id: 2, text: 'Handy' },
+  { id: 3, text: 'Macbook' },
+]); // zu ref(null) ändern nach vorstellen 2
 </script>
 
 <template>
   <header>
-    <h1>Meine Vue Notizenliste</h1>
+    <h1>Meine Produktliste</h1>
   </header>
 
   <main class="notes-content">
+
+    // Schleife, bei Wiederholung dem Alias Note zugewiesen (nötig für vue)
     <div
       v-for="note in notes":key="note.id" >
+      // übergibt Notiz an Child Component
       <NoteItem :note-data="note" />
     </div>
   </main>
--- meine-vue-app/src/components/Noteitem.vue
+++ meine-vue-app/src/components/Noteitem.vue
@@ -1,9 +1,11 @@
 <script setup>
 import {defineProps} from 'vue';
+// definiert externe Schnittstelle und das ein Objekt benötigt wird
 const props = defineProps({noteData: {type: Object, required: true}});
 </script>
 
 <template>
+  // zeigt Wert der Eigenschaft text dieses Objekts an
  <div class="note-item" >
    <p>{{ noteData.text }}</p>
  </div>
