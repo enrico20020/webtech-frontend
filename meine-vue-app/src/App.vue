d9a688502da0617f6092e05b8328bc819d4a8bba
macbook <enricoschweigert@gmail.com>
2025-12-04 19:24:34 +0100

    Deadline 4 finale Version

--- meine-vue-app/src/App.vue
+++ meine-vue-app/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NoteItem from './components/Noteitem.vue';
 import {ref} from 'vue';
+import axios from "axios";
 
 const Backend_URL = 'https://webtech-backend-6ot9.onrender.com'
 
@@ -10,6 +11,38 @@ const notes = ref([
   { id: 3, text: 'Macbook' },
 ]); // zu ref(null) ändern nach vorstellen 2
 
+
+//POST-Route
+async function saveNewProduct(name, price) {
+  const url = `${Backend_URL}/api/products`;
+
+const dataToSend = {
+  name: name,
+  price: price
+};
+
+try {
+  const response = await axios.post(url, dataToSend, {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+
+  if (response.status === 201) {
+    const savedProduct = response.data;
+    console.log("Produkt gespeichert:", savedProduct);
+
+    return savedProduct;
+
+  } else {
+    console.error("Speicherfehler:", response.status);
+  }
+} catch (error) {
+  console.error("Netzwerk- oder CORS-Fehler:", error);
+}
+}
+
+saveNewProduct('Maus', 40);
 
 
 </script>
